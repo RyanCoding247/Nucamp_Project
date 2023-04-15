496e0bcc87f254e46eee452d98cce332932e9ac9
Ryan Nichols <kastrom247@gmail.com>
2023-04-15 14:19:03 -0500

    Week 4 Workshop Corrected

--- routes/favoriteRouter.js
+++ routes/favoriteRouter.js
@@ -100,41 +100,10 @@ favoriteRouter.route('/:campsiteId')
             } else {
                 res.statusCode = 200;
                 res.setHeader('Content-Type', 'text/plain');
-                res.end('"That campsite is already in the list of favorites!"')
+                res.end("That campsite is already in the list of favorites!")
             }
         }).catch(err => next(err));
     })
-    /*
-    .post(cors.corsWithOptions, authenticate.verifyUser, (req, res, next) => {
-        Favorite.findOne({user: req.user._id})
-        .then(favorite => {
-            if (favorite) {
-                if (!favorite.campsites.includes(req.params.campsiteId)) {
-                    favorite.campsites.push(req.params.campsiteId);
-                    favorite.save()
-                    .then(favorite => {
-                        res.statusCode = 200;
-                        res.setHeader('Content-Type', 'application/json');
-                        res.json(favorite);
-                    })
-                    .catch(err => next(err));
-                } else {
-                    res.statusCode = 200;
-                    res.setHeader('Content-Type', 'text/plain');
-                    res.end('That campsite is already a favorite!');
-                }
-            } else {
-                Favorite.create({ user: req.user._id, campsites: [req.params.campsiteId] })
-                .then(favorite => {
-                    res.statusCode = 200;
-                    res.setHeader('Content-Type', 'application/json');
-                    res.json(favorite);
-                })
-                .catch(err => next(err));
-            }
-        })
-        .catch(err => next(err));
-    })*/
     .put(cors.corsWithOptions, authenticate.verifyUser, (req, res) => {
         res.statusCode = 403;
         res.end(`PUT operation not supported on /campsites/${req.params.campsiteId}`);
